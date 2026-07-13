3596b294b6d95e25ef426dbbb7e0777906c8c69d
Harshal Patil <151955928+Harshal4257@users.noreply.github.com>
2026-07-13 11:15:33 +0530

    Time: 67 ms (32.51%), Space: 111.3 MB (13.13%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for(auto x:nums){
+            if(seen.count(x)){
+                return true;
+                break;
+            }
+            seen.insert(x);
+        }
+        return false;
+    }
+};
