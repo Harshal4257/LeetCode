70dbb8c64b1e2e39d733af6a02e18dd756408f95
Harshal Patil <151955928+Harshal4257@users.noreply.github.com>
2026-07-13 17:22:57 +0530

    Time: 80 ms (5.43%), Space: 34.1 MB (11.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
-        for(string& s:strs){
+        for(string s:strs){
             int count[26] = {0};
             for(char c:s) count[c - 'a']++;
 
