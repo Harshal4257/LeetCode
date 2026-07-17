e7ca86e9f514b6b0077a6f933bca2664ce58baf9
Harshal Patil <151955928+Harshal4257@users.noreply.github.com>
2026-07-17 11:27:02 +0530

    Time: 0 ms (100%), Space: 62.8 MB (79.02%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        if(height.empty()) return 0;
         int marea = 0;
         int left = 0;
         int right = height.size()-1;
