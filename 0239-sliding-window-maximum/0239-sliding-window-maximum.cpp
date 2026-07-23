7f9f368a50cc8daac699317c229176bba81174ce
Harshal Patil <151955928+Harshal4257@users.noreply.github.com>
2026-07-23 12:20:57 +0530

    Time: 387 ms (5.39%), Space: 216.6 MB (5.01%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        multiset<int> window;
+        vector<int> ans;
+        for(int i = 0;i < k;i++) window.insert(nums[i]);
+        ans.push_back(*window.rbegin());
+
+        for(int i = k ;i < nums.size();i++){
+            window.erase(window.find(nums[i-k]));
+            window.insert(nums[i]);
+            ans.push_back(*window.rbegin());
+        }
+        return ans;
+    }
+};
